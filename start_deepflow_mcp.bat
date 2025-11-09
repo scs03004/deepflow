cf89f4dd26e9c619a98f1d824a9f1124e738c98e
Sebastian <20294508+scs03004@users.noreply.github.com>
2025-11-08 21:44:02 -0600

    doc update and feature add

--- start_deepflow_mcp.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Sebastian\PycharmProjects\npcgpt-dependency\dependency-toolkit"
-set PYTHONPATH=C:\Users\Sebastian\PycharmProjects\npcgpt-dependency\dependency-toolkit
-"C:\Users\Sebastian\PycharmProjects\npcgpt-dependency\NPCGPT\.venv\Scripts\python.exe" -m deepflow.mcp.server
